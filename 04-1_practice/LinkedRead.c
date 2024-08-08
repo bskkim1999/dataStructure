6d0ede86de721a3f6814a19052e91963b1137292
bskkim1999 <dbjewelrydb@gmail.com>
2024-08-08 17:36:02 +0900

    lklk

--- 04-1_practice/LinkedRead.c
+++ 04-1_practice/LinkedRead.c
@@ -37,7 +37,43 @@ int main(void){
     printf("\n\n");
 
     //입력 받은 데이터의 출력과정//
-    
+    printf("입력 받은 데이터의 전체출력 \n");
+
+    if(head==NULL){
+        printf("저장된 자연수가 존재하지 않는다. \n");
+    }
+    else{
+        cur = head;
+        printf("%d ", cur->data);
+
+        while(cur->next != NULL){
+            cur = cur->next;
+            printf("%d ", cur->data);
+        }
+    }
+
+    printf("\n\n");
+
+    //메모리의 해제과정//
+    if(head == NULL){
+        return 0;
+    }
+    else{
+        Node* delNode = head;
+        Node* delNextNode = head->next;
+
+        printf("%d을 삭제합니다. \n", head->data);
+        free(delNode);
+
+        while(delNextNode != NULL){
+            delNode = delNextNode;
+            delNextNode = delNextNode->next;
+
+            printf("%d을 삭제합니다. \n", delNode->data);
+            free(delNode);
+        }
+    }
+
 
 
 
