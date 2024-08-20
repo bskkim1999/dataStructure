9a57f4a714003ebdbc6835717de4e72f6905a2b6
bskkim1999 <dbjewelrydb@gmail.com>
2024-08-20 22:15:18 +0900

    hello

---
+++ 04-2/build/CMakeFiles/program_exe_file.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/program_exe_file.dir/DLinkedRead.c.obj"
+  "CMakeFiles/program_exe_file.dir/DLinkedRead.c.obj.d"
+  "libprogram_exe_file.dll.a"
+  "program_exe_file.exe"
+  "program_exe_file.exe.manifest"
+  "program_exe_file.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/program_exe_file.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ 04-2/build/CMakeFiles/program_exe_file.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for program_exe_file.
+# This may be replaced when dependencies are built.
